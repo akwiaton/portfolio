1690fe27b246f47941054c9e747be84a6b1de263
Alicja K <akwiaton@gmail.com>
2024-06-12 10:46:48 +0200

    function onSubmit added, sendMail.php added

--- src/app/main-component/contact/contact.component.ts
+++ src/app/main-component/contact/contact.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
+  contactData = {
+      name: "",
+      email: "",
+      message: "",
+    }
+
+    mailTest = true; // wenn auf dem server auf false setzen
+
+    post = {
+      endPoint: 'https://deineDomain.de/sendMail.php',
+      body: (payload: any) => JSON.stringify(payload),
+      options: {
+        headers: {
+          'Content-Type': 'text/plain',
+          responseType: 'text',
+        },
+      },
+    };
+  // if else nur zum testen, danach entfernen
+    onSubmit(ngForm: NgForm) {
+      if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+        this.http.post(this.post.endPoint, this.post.body(this.contactData))
+          .subscribe({
+            next: (response) => {
+  // hier kann ich hizufügen was noch passieren soll
+              ngForm.resetForm();
+            },
+            error: (error) => {
+              console.error(error);
+            },
+            complete: () => console.info('send post complete'),
+          });
+      } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+  // zum testen das selbe hier einfügen
+        ngForm.resetForm();
+      }
+    }
 }
