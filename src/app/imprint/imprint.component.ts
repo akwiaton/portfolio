036cc2b74dce33d4c51718af75dbe36a2bc9fe8b
Alicja K <akwiaton@gmail.com>
2024-06-18 12:11:38 +0200

    upadate

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { LandingpageComponent } from '../main-component/landingpage/landingpage.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, NavbarComponent, FooterComponent, LandingpageComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
