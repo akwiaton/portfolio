65a446c5c74e82550bf7e8b1d70bd544d39cbd75
Alicja K <akwiaton@gmail.com>
2024-06-24 16:09:53 +0200

    translate service popup added; translate service privat policy section one added

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { LandingpageComponent } from '../main-component/landingpage/landingpage.component';
+import { TranslationService } from '../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [RouterModule, CommonModule, NavbarComponent, FooterComponent, LandingpageComponent],
+  imports: [RouterModule, CommonModule, NavbarComponent, FooterComponent, LandingpageComponent, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+  translate = Inject(TranslationService);
 }
--- src/app/main-component/contact/popup/popup.component.ts
+++ src/app/main-component/contact/popup/popup.component.ts
@@ -1,17 +1,21 @@
 import { Component, Inject } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
+import { TranslationService } from '../../../translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
 @Component({
   selector: 'app-popup',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, TranslateModule],
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss'
 })
 export class PopupComponent {
+
+  translate = Inject(TranslationService);
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
 
